70136305d07709cfd77deaec8377c9d66b0fc4f7
Kaito Kudou <kaitokudou2468110921@gmail.com>
2022-03-24 18:14:37 +0900

    [add]presenter作成

---
+++ Yumemi-ios-training/Yumemi-ios-training/Presenter/WeatherPresenter.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherPresenter.swift
+//  Yumemi-ios-training
+//
+//  Created by 工藤 海斗 on 2022/03/24.
+//
+
+import Foundation
+
+protocol WeatherPresenterProtocolInput {
+    func didTapReloadButton() -> String
+}
+
+protocol WeatherPresenterProtocolOutput {
+    func showWeather()
+}
+
+class WeatherPresenter: WeatherPresenterProtocolInput {
+    var view: WeatherPresenterProtocolOutput!
+    var model: WeatherModelProtocol!
+    
+    init(view: WeatherPresenterProtocolOutput, model: WeatherModelProtocol) {
+        self.view = view
+        self.model = model
+    }
+    
+    func didTapReloadButton() -> String {
+        return model.fetchWeather()
+    }
+}
