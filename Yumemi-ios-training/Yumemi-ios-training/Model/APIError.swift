92fd52a03afe559bc8909384b19bcd1acf6adb32
Kaito Kudou <kaitokudou2468110921@gmail.com>
2022-03-24 16:42:40 +0900

    [add]エラーを定義

---
+++ Yumemi-ios-training/Yumemi-ios-training/Model/APIError.swift
@@ -0,0 +1,22 @@
+//
+//  APIError.swift
+//  Yumemi-ios-training
+//
+//  Created by 工藤 海斗 on 2022/03/24.
+//
+
+import Foundation
+
+enum APIError {
+    case invalidParameterError
+    case unknownError
+    
+    var errorMessage: String {
+        switch self {
+        case .invalidParameterError:
+            return "パラメータが無効"
+        case .unknownError:
+            return "予期せぬエラーが発生"
+        }
+    }
+}
