e8cf1fe44dcae04fa8825952880312c7af633dbd
Kaito Kudou <kaitokudou2468110921@gmail.com>
2022-03-24 18:14:56 +0900

    [add]天気の画像をimageViewに表示

--- Yumemi-ios-training/Yumemi-ios-training/View/WeatherViewController.swift
+++ Yumemi-ios-training/Yumemi-ios-training/View/WeatherViewController.swift
@@ -10,14 +10,39 @@ import UIKit
 class WeatherViewController: UIViewController {
 
     @IBOutlet weak var weatherImageView: UIImageView!
+    private var presenter: WeatherPresenterProtocolInput!
+    private var weatherString: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        presenter = WeatherPresenter(view: self, model: WeatherModel())
+        inject(presenter: presenter)
+    }
+    
+    func inject(presenter: WeatherPresenterProtocolInput) {
+        self.presenter = presenter
     }
     
     @IBAction func reloadWeather(_ sender: Any) {
+        weatherString = presenter.didTapReloadButton()
+        switch weatherString {
+        case "sunny":
+            weatherImageView.image = R.image.sunny()
+            weatherImageView.tintColor = R.color.red()
+        case "cloudy":
+            weatherImageView.image = R.image.cloudy()
+            weatherImageView.tintColor = R.color.gray()
+        case "rainy":
+            weatherImageView.image = R.image.rainy()
+            weatherImageView.tintColor = R.color.blue()
+        default:
+            return
+        }
     }
     
+}
+
+extension WeatherViewController: WeatherPresenterProtocolOutput {
+    func showWeather() {
+    }
 }
