f4107c00d6985d3a5a9038e0f38f138276bfb78a
Kaito Kudou <kaitokudou2468110921@gmail.com>
2022-03-24 16:05:00 +0900

    [add]WeatherViewController.swiftを追加

---
+++ Yumemi-ios-training/Yumemi-ios-training/View/WeatherViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherViewController.swift
+//  Yumemi-ios-training
+//
+//  Created by 工藤 海斗 on 2022/03/24.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
