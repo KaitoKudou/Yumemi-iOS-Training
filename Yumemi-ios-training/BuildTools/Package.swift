960c60738989d138228ee979c6a0e6a25e2733a8
Kaito Kudou <kaitokudou2468110921@gmail.com>
2022-03-25 17:47:00 +0900

    [fix]R.swiftのバージョンを6.1.0に修正

--- Yumemi-ios-training/BuildTools/Package.swift
+++ Yumemi-ios-training/BuildTools/Package.swift
@@ -4,6 +4,6 @@ import PackageDescription
 let package = Package(
     name: "BuildTools",
     dependencies: [
-        .package(url: "https://github.com/mac-cain13/R.swift", from: "5.1.0")],
+        .package(url: "https://github.com/mac-cain13/R.swift", from: "6.1.0")],
     targets: [.target(name: "BuildTools", path: "")]
 )
