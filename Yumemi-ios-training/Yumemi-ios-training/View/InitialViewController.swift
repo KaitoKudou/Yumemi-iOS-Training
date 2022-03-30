184a3008504acb2e052037f4b547f0121392c304
Kaito Kudou <kaitokudou2468110921@gmail.com>
2022-03-30 17:53:13 +0900

    [add]新しいViewControllerを追加

---
+++ Yumemi-ios-training/Yumemi-ios-training/View/InitialViewController.swift
@@ -0,0 +1,15 @@
+//
+//  InitialViewController.swift
+//  Yumemi-ios-training
+//
+//  Created by 工藤 海斗 on 2022/03/30.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
