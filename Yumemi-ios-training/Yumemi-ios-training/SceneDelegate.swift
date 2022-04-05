086b475770d043c5545e6e8c71e0c3bc55b773e9
Kaito Kudou <kaitokudou2468110921@gmail.com>
2022-04-05 12:41:00 +0900

    [remove]rootViewControllerの設定を削除

--- Yumemi-ios-training/Yumemi-ios-training/SceneDelegate.swift
+++ Yumemi-ios-training/Yumemi-ios-training/SceneDelegate.swift
@@ -17,8 +17,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
-        let initialViewController = R.storyboard.main.initialViewController()
-        window?.rootViewController = initialViewController
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
