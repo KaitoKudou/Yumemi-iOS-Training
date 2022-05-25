e15bb43aa0fd7bc082a8426f95c52516453c8fb7
Kaito Kudou <kaitokudou2468110921@gmail.com>
2022-05-25 23:17:29 +0900

    [fix]テストを修正

--- Yumemi-ios-training/Yumemi-ios-trainingTests/WeatherViewTests.swift
+++ Yumemi-ios-training/Yumemi-ios-trainingTests/WeatherViewTests.swift
@@ -10,73 +10,61 @@ import XCTest
 
 class WeatherViewTests: XCTestCase {
 
-    var model: WeatherFetcher!
-    private let viewController = R.storyboard.main.weatherViewController()
+    var model: WeatherListFetcher!
+    private let weatherViewController = R.storyboard.main.weatherViewController()
+    private let weatherTableViewCell = Bundle(for: WeatherTableViewCell.self).loadNibNamed(R.nib.weatherTableViewCell.name, owner: nil)?.first as! WeatherTableViewCell
     
     override func setUpWithError() throws {
         let scenes = UIApplication.shared.connectedScenes
         let windowScenes = scenes.first as? UIWindowScene
-        windowScenes?.keyWindow?.rootViewController = viewController
-        model = WeatherFetcher()
+        windowScenes?.keyWindow?.rootViewController = weatherViewController
+        model = WeatherListFetcher()
     }
 
     override func tearDownWithError() throws {
     }
     
-    func testCallFetchWeather() {
-        viewController?.reloadButton.sendActions(for: .touchUpInside)
-        XCTAssertEqual(viewController?.activityIndicatorView.isAnimating, true) // インジケータが表示されるとpresenter.fetchWeather()が呼ばれていることがわかる
-    }
-    
     @MainActor func testShowInvalidParameterErrorAlert() {
-        viewController?.showErrorAlert(with: R.string.message.invalidParameterError())
-        XCTAssertTrue(viewController?.presentedViewController is UIAlertController)
-        XCTAssertEqual(viewController?.presentedViewController?.title, R.string.message.alertControllerTitle())
-        XCTAssertEqual(viewController?.activityIndicatorView.isAnimating, false)
-        XCTAssertEqual((viewController?.presentedViewController as? UIAlertController)?.message, R.string.message.invalidParameterError())
+        weatherViewController?.showErrorAlert(with: R.string.message.invalidParameterError())
+        XCTAssertTrue(weatherViewController?.presentedViewController is UIAlertController)
+        XCTAssertEqual(weatherViewController?.presentedViewController?.title, R.string.message.alertControllerTitle())
+        XCTAssertEqual((weatherViewController?.presentedViewController as? UIAlertController)?.message, R.string.message.invalidParameterError())
     }
     
     @MainActor func testShowUnknownErrorAlert() {
-        viewController?.showErrorAlert(with: R.string.message.unknownError())
-        XCTAssertTrue(viewController?.presentedViewController is UIAlertController)
-        XCTAssertEqual(viewController?.presentedViewController?.title, R.string.message.alertControllerTitle())
-        XCTAssertEqual(viewController?.activityIndicatorView.isAnimating, false)
-        XCTAssertEqual((viewController?.presentedViewController as? UIAlertController)?.message, R.string.message.unknownError())
+        weatherViewController?.showErrorAlert(with: R.string.message.unknownError())
+        XCTAssertTrue(weatherViewController?.presentedViewController is UIAlertController)
+        XCTAssertEqual(weatherViewController?.presentedViewController?.title, R.string.message.alertControllerTitle())
+        XCTAssertEqual((weatherViewController?.presentedViewController as? UIAlertController)?.message, R.string.message.unknownError())
     }
     
     @MainActor func testShowSunnyImageWhenResponseIsSunny() {
-        viewController?.showWeather(weatherResponse: WeatherResponse(weather: .sunny, maxTemp: 0, minTemp: 0, date: Date()))
-        XCTAssertNotNil(viewController?.weatherImageView)
-        XCTAssertEqual(viewController?.weatherImageView.image, R.image.sunny())
-        XCTAssertEqual(viewController?.activityIndicatorView.isAnimating, false)
+        weatherTableViewCell.configure(weatherListResponse: WeatherListResponse(info: WeatherResponse(weather: .sunny, maxTemp: 0, minTemp: 0, date: Date()), area: "Tokyo"))
+        XCTAssertNotNil(weatherTableViewCell.weatherImageView)
+        XCTAssertEqual(weatherTableViewCell.weatherImageView.image, R.image.sunny())
     }
     
     @MainActor func testShowCloudyImageWhenResponseIsCloudy() {
-        viewController?.showWeather(weatherResponse: WeatherResponse(weather: .cloudy, maxTemp: 0, minTemp: 0, date: Date()))
-        XCTAssertNotNil(viewController?.weatherImageView)
-        XCTAssertEqual(viewController?.weatherImageView.image, R.image.cloudy())
-        XCTAssertEqual(viewController?.activityIndicatorView.isAnimating, false)
+        weatherTableViewCell.configure(weatherListResponse: WeatherListResponse(info: WeatherResponse(weather: .cloudy, maxTemp: 0, minTemp: 0, date: Date()), area: "Tokyo"))
+        XCTAssertNotNil(weatherTableViewCell.weatherImageView)
+        XCTAssertEqual(weatherTableViewCell.weatherImageView.image, R.image.cloudy())
     }
     
     @MainActor func testShowRainyImageWhenResponseIsRainy() {
-        viewController?.showWeather(weatherResponse: WeatherResponse(weather: .rainy, maxTemp: 0, minTemp: 0, date: Date()))
-        XCTAssertNotNil(viewController?.weatherImageView)
-        XCTAssertEqual(viewController?.weatherImageView.image, R.image.rainy())
-        XCTAssertEqual(viewController?.activityIndicatorView.isAnimating, false)
+        weatherTableViewCell.configure(weatherListResponse: WeatherListResponse(info: WeatherResponse(weather: .rainy, maxTemp: 0, minTemp: 0, date: Date()), area: "Tokyo"))
+        XCTAssertNotNil(weatherTableViewCell.weatherImageView)
+        XCTAssertEqual(weatherTableViewCell.weatherImageView.image, R.image.rainy())
     }
     
     @MainActor func testShowTemperatureLabel() {
-        viewController?.showWeather(weatherResponse: WeatherResponse(weather: .sunny, maxTemp: 10, minTemp: 5, date: Date()))
-        XCTAssertNotNil(viewController?.maxTemperatureLabel)
-        XCTAssertNotNil(viewController?.minTemperatureLabel)
-        XCTAssertEqual(viewController?.maxTemperatureLabel.text, "10")
-        XCTAssertEqual(viewController?.minTemperatureLabel.text, "5")
-        XCTAssertEqual(viewController?.activityIndicatorView.isAnimating, false)
+        weatherTableViewCell.configure(weatherListResponse: WeatherListResponse(info: WeatherResponse(weather: .rainy, maxTemp: 10, minTemp: 5, date: Date()), area: "Tokyo"))
+        XCTAssertEqual(weatherTableViewCell.minTemperatureLabel.text, "5")
+        XCTAssertEqual(weatherTableViewCell.maxTemperatureLabel.text, "10")
     }
     
     func testResponseUnknownError() async {
         model.jsonDecoder.dateDecodingStrategy = .secondsSince1970 // iso8601以外を指定
-        switch await model.fetchWeather() {
+        switch await model.fetchWeather(areas: ["Tokyo"], date: Date()) {
         case .success(_):
             XCTFail("\(#function) fail")
         case .failure(let error):
@@ -87,7 +75,7 @@ class WeatherViewTests: XCTestCase {
     
     func testResponseInvalidParameterError() async {
         model.jsonEncoder.dateEncodingStrategy = .secondsSince1970 // iso8601以外を指定
-        switch await model.fetchWeather() {
+        switch await model.fetchWeather(areas: ["Tokyo"], date: Date()) {
         case .success(_):
             XCTFail("\(#function) fail")
         case .failure(let error):
